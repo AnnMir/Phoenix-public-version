a16d19c678c6c3665d55fabfbd349dd8bdb9e708
Christina Marchenko <kristina9607@mail.ru>
2018-05-01 22:47:18 +0700

    Remove folder creation on GD if it already exists

--- app/src/main/java/nsu/fit/g14201/marchenko/phoenix/cloud/GoogleDriveAPI.java
+++ app/src/main/java/nsu/fit/g14201/marchenko/phoenix/cloud/GoogleDriveAPI.java
@@ -9,15 +9,25 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveClient;
 import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
 import com.google.android.gms.drive.DriveResourceClient;
+import com.google.android.gms.drive.Metadata;
 import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SearchableField;
+import com.google.android.gms.tasks.Task;
 
 import nsu.fit.g14201.marchenko.phoenix.App;
 import nsu.fit.g14201.marchenko.phoenix.connection.SignInException;
 
 public class GoogleDriveAPI implements CloudAPI {
+    private static final String FOLDER_NAME = App.getAppName();
+
     private DriveClient driveClient;
     private DriveResourceClient driveResourceClient;
+    private DriveId appFolderId;
+    private DriveFolder parentFolder;
 
     public GoogleDriveAPI(Context context) throws SignInException {
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(context);
@@ -32,23 +42,46 @@ public class GoogleDriveAPI implements CloudAPI {
 
     @Override
     public void createAppFolderIfNotExists() {
-        // TODO NEXT: Add checkup if folder exits
-        // TODO: Move to background
+        // TODO: Move listeners to background
         // TODO: Add outer listeners
         driveResourceClient
                 .getRootFolder()
                 .continueWithTask(task -> {
-                    DriveFolder parentFolder = task.getResult();
-                    MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
-                            .setTitle(App.getAppName())
-                            .setMimeType(DriveFolder.MIME_TYPE)
-                            .build();
-                    return driveResourceClient.createFolder(parentFolder, changeSet);
+                    Query query = new Query.Builder()
+                        .addFilter(Filters.and(
+                                Filters.eq(SearchableField.MIME_TYPE, "application/vnd.google-apps.folder"),
+                                Filters.eq(SearchableField.TITLE, FOLDER_NAME),
+                                Filters.eq(SearchableField.TRASHED, false)))
+                        .build();
+                    parentFolder = task.getResult();
+                    return driveResourceClient.queryChildren(parentFolder, query);
+
                 })
-                .addOnSuccessListener(driveFolder -> Log.d(App.getTag(), "Created app folder"))
+                .addOnSuccessListener(metadataBuffer -> {
+                    for (Metadata metadata : metadataBuffer) {
+                        if (metadata.getTitle().equals(FOLDER_NAME)) {
+                            Log.d(App.getTag(), "Folder already exists");
+                            appFolderId = metadata.getDriveId();
+                            return;
+                        }
+                    }
+                    createAppFolder()
+                        .addOnFailureListener(e -> {
+                            Log.e(App.getTag(), "Failed to create app folder");
+                            e.printStackTrace();
+                        });
+                })
                 .addOnFailureListener(e -> {
-                    Log.d(App.getTag(), "Failed to create add folder");
+                    Log.e(App.getTag(), "Failed to create app folder");
                     e.printStackTrace();
                 });
+    }
+
+    private Task<DriveFolder> createAppFolder() {
+        MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                .setTitle(App.getAppName())
+                .setMimeType(DriveFolder.MIME_TYPE)
+                .build();
+        return driveResourceClient.createFolder(parentFolder, changeSet);
     }
 }
